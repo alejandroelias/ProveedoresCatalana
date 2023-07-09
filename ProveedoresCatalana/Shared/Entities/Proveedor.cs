322d00a16d15ee2a6247dc841b486e9141eb7d1f
Alejandro Elias <alejandro.elias@outlook.com>
2023-07-08 20:04:11 -0600

    Add project files.

---
+++ ProveedoresCatalana/Shared/Entities/Proveedor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProveedoresCatalana.Shared.Entities
+{
+    public class Proveedor
+    {
+        public int CodInter { get; set; }
+        public string CodProv { get; set; }
+        public string Nombre { get; set; }
+
+        //Extran
+        //Direccion
+        //Registro
+        //Giro
+        //Telefono
+        //Fax
+        //Email
+        //CodPais
+        //Codarea
+        //Contacto
+        //NoCuenta
+        //Plazo
+        //Bor
+        //CodTipoPag
+        //Nit
+        //Depto
+        //Municipio
+        //CodContribuyente
+        //CodBanco
+        //NoCuentaBancaria
+        //TipoCuentaBancaria
+        //RetenerRenta
+        //PorcentajeRetencion
+        //ContribuyOld
+        //InactivoProv
+        //NombreNIT
+        //TipoOperacionF987
+        //tipoDocumento
+        //codActividad
+        //tipo_item
+        //datos_completos_mh
+        //DUI
+        //Usuario_edit
+
+    }
+}
